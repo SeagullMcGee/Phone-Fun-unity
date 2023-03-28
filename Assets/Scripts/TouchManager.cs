3d539675d0ca5ef9b91add121f60570bf0075a4e
Malthe Hartvigsen <113890281+SeagullMcGee@users.noreply.github.com>
2023-03-28 10:50:16 +0200

    sådan

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class TouchManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    private PlayerInput playerInput;
+
+    private InputAction touchPositionAction;
+    private InputAction touchPressAction;
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPositionAction = playerInput.actions.FindAction("TouchPosition");
+        touchPressAction = playerInput.actions.FindAction("TouchPress");
+
+    }
+
+    private void OnEnable()
+    {
+        touchPressAction.performed += TouchPressed;
+
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= TouchPressed;
+    }
+
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        Vector3 position = Camera.main.ScreenToWorldPoint(touchPositionAction.
+            ReadValue<Vector2>());
+        position.z = player.transform.position.z;
+        player.transform.position = position;
+    }
+    
+
+   
+    
+}
